e5ceb850811073b8a5309fc931c3d19f46a7ac32
VenomFeed <93274199+vatari4@users.noreply.github.com>
2022-12-19 22:18:11 +0700

    Create 1.3.py

---
+++ python1/1.3.py
@@ -0,0 +1,7 @@
+firstNumb = input()    #Вводится 1-3 числа
+secondNumb = input()
+thirdNumb = input()
+if firstNumb == "раз" and secondNumb == "два" and thirdNumb == "три": #Условие 
+  print('гори')    #если условие выполняется выводит "гори"
+else:
+  print('Не гори') #если условие не выполняется выводит "Не гори"

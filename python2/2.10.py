0b7b61d44adb52e6051e55add3cfd63e25b43cfa
VenomFeed <93274199+vatari4@users.noreply.github.com>
2022-12-20 00:22:26 +0700

    Update 2.10.py

--- python2/2.10.py
+++ python2/2.10.py
@@ -1,16 +1,16 @@
 firstStr = float(input("Введите число"))      #1 строка
 secondStr = float(input("Введите число"))     #2 строка
 thirdStr = str(input("Введите строку"))       #3 строка
-if "-" in thirdStr:                  #если в строке встречается "-" то из 1 строки 
+if "-" in thirdStr:                  #если в строке встречается "-" - 1 строка - 2 строка
   firstStr -= secondStr
   print(firstStr)
-elif("+" in thirdStr):
+elif("+" in thirdStr):               #если в строке встречается "+" - 1 строка + 2 строка 
   firstStr += secondStr
   print(firstStr)
-elif("*" in thirdStr):
+elif("*" in thirdStr):               #если в строке встречается "*" - 1 строка * 2 строка
   firstStr *= secondStr
   print(firstStr)
-elif("/" in thirdStr):
+elif("/" in thirdStr):               #если в строке встречается "*" - 1 строка / 2 строка
   firstStr /= secondStr
   print(firstStr)
 else:

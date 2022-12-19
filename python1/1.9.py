2dbc35d8ad282faf378a6ce4fc3aeb33e80fef57
VenomFeed <93274199+vatari4@users.noreply.github.com>
2022-12-19 22:40:37 +0700

    Update 1.9.py

--- python1/1.9.py
+++ python1/1.9.py
@@ -1,2 +1,10 @@
-log = input()
+log = input('введите ваш логин')   #пользователь вводит логин
 if "@" in(log):
+  print('Некорректный логин')
+else:
+  print('ok')
+adress = input('введите ваш email') #пользователь вводит почту
+if "@" in adress:
+  print("Ok")
+else:
+   print('Некорректный логин')

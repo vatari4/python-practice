aef8e287596912be9300355a1eafa6901fcfcb63
VenomFeed <93274199+vatari4@users.noreply.github.com>
2022-12-19 22:10:13 +0700

    Update 1.2.py
    Создаём переменную python в которое пользователь вводит любое значение, при вводе "Python" пользователю выводит ответ "Yes", в ином случае "No"

--- python1/1.2.py
+++ python1/1.2.py
@@ -1,5 +1,5 @@
-python = input('Введите Python')
-if python == 'Python':
+python = input('Введите Python')  #Создаём переменную python в которое пользователь вводит любое значение
+if python == 'Python':            #при вводе "Python" пользователю выводит ответ "Yes", в ином случае "No"
   print('Yes')
 else:
   print('No')

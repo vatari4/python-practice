94d2a529a1d1de39d14805a010a922b030cf1d87
VenomFeed <93274199+vatari4@users.noreply.github.com>
2022-12-20 00:03:34 +0700

    Create 2.9.py

---
+++ python2/2.9.py
@@ -0,0 +1,35 @@
+firstHight = str(input())
+secondHight = str(input())
+thirdHight = str(input())
+
+ 
+
+if firstHight > secondHight and secondHight > thirdHight:
+print(firstHight)
+print(secondHight)
+print(thiraaignt)
+
+elif firstHight > thirdHight and thirdHight > secondHight:
+print(firstHight)
+print(thirdHight)
+print(secondHight)
+
+elif secondHight > firstHight and firstHight > thirdHight:
+print(secondHight)
+print(firstHight)
+print(thirdHight)
+
+elif secondHight > thirdHight and thirdHight > firstHight:
+print(secondHight}
+print(thirdHight)
+print(firstHight)
+
+elif thirdHight > secondHight and secondHight > firstHight:
+print(thirdHight)
+print(secondHight)
+print(firstHight)
+
+elif thirdHight > firstHight and firstHight > secondHight:
+print(thirdHight)
+print(firstHight)
+print(secondHight)

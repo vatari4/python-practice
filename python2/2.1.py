4ffc4821c05a2ab6a4ad9145b73693f42095aa3e
VenomFeed <93274199+vatari4@users.noreply.github.com>
2022-12-19 23:29:21 +0700

    Update 2.1.py

--- python2/2.1.py
+++ python2/2.1.py
@@ -1,6 +1,6 @@
-firstCity = input("Выберете город в котором Женя будет в Июле")
-secondCity = input("Выберете город в котором Женя будет в Июле")
-if(firstCity == "Пенза" and secondCity == "Тула")or(firstCity == "Тула" and secondCity == "Пенза"):
-  print("No")
-elif(firstCity == "Пенза" and secondCity != "Пенза")or(firstCity == "Тула" and secondCity != "Тула"):
+firstCity = input("Выберете город в котором Женя будет в Июле")    #первый город
+secondCity = input("Выберете город в котором Женя будет в Августе")   #второй город
+if(firstCity == "Пенза" and secondCity == "Тула")or(firstCity == "Тула" and secondCity == "Пенза"):   #если первый город пенза или тула, а второй тула или пенза - то выводит "No"
+  print("No")                                                                                         
+elif(firstCity == "Пенза" and secondCity != "Пенза")or(firstCity == "Тула" and secondCity != "Тула"): #если первый город пенза или тула, а второй не тула и не пенза - то выводит "yes"
   print("yes")

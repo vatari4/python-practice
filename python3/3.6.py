0921e636ea01fe57d2ddb5fe1ee6d9260340674a
VenomFeed <93274199+vatari4@users.noreply.github.com>
2022-12-20 01:06:44 +0700

    Update 3.6.py

--- python3/3.6.py
+++ python3/3.6.py
@@ -1,11 +1,11 @@
 firstPasword = input()
 secondPasword = input()
-if(len(firstPasword) < 8):
+if(len(firstPasword) < 8):                  #если введёный паоль меньше 8 то вводится ещё пара паролей
   print("Короткий пароль")
   
   firdPasword = input()
   fourthPasword = input()
-  if(len(firdPasword) < 8):
+  if(len(firdPasword) < 8):                 #таже проверка на кол-во символов в пароле
     print("Короткий пароль")
   elif(firdPasword != fourthPasword)
   

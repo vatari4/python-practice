ef2c9b1f84edef7896aabae18583db95e531faeb
VenomFeed <93274199+vatari4@users.noreply.github.com>
2022-12-19 22:29:54 +0700

    Update 1.6.py

--- python1/1.6.py
+++ python1/1.6.py
@@ -1,1 +1,15 @@
-firstQuestion = input()
+firstQuestion = input("Любите ли вы котиков")   #первый вопрос
+if(firstQuestion == "да"):    
+  print("я тоже!")                              
+elif(firstQuestion == "нет"):
+  print("а я люблю")
+else:
+  print("ERROR, нужно ответить да или нет")     #выводит ошибку и просит пользователя ввести нужные данные
+secondQuestion = input("Умеете ли вы программировать") #второй вопрос
+if(secondQuestion == "да"):
+  print("это прекрасно!")
+elif(secondQuestion == "нет"):
+  print("Как жаль!, но у вас ещё есть время научиться!")
+else:
+  print("ERROR, нужно ответить да или нет")
+ 

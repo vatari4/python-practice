c2cb1ce4dfe14b93df4815b07017b124bb3560d4
VenomFeed <93274199+vatari4@users.noreply.github.com>
2022-12-20 00:09:39 +0700

    Create 2.10.py

---
+++ python2/2.10.py
@@ -0,0 +1,5 @@
+firstStr = float(input("Введите число"))      #1 строка
+secondStr = float(input("Введите число"))     #2 строка
+thirdStr = str(input("Введите строку"))       #3 строка
+if "-" in thirdStr:
+  

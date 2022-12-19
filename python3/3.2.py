0bbd99d447ccc2b36c8678f8c97ca48adcfda011
VenomFeed <93274199+vatari4@users.noreply.github.com>
2022-12-20 00:50:49 +0700

    Create 3.2.py

---
+++ python3/3.2.py
@@ -0,0 +1,8 @@
+firstPasword = input()
+secondPasword = input()
+if(len(firstPasword) < 8):
+  print("Короткий пароль")
+elif(firstPasword != secondPasword)
+  print("пароль не совпадает")
+else:
+  print("ok")

a38fc8c4da1d5dce7b7fa1f85ae07bc2a8616052
VenomFeed <93274199+vatari4@users.noreply.github.com>
2022-12-19 23:26:39 +0700

    Update 2.1.py

--- python2/2.1.py
+++ python2/2.1.py
@@ -1,1 +1,6 @@
-
+firstCity = input("Выберете город в котором Женя будет в Июле")
+secondCity = input("Выберете город в котором Женя будет в Июле")
+if(firstCity == "Пенза" and secondCity == "Тула")or(firstCity == "Тула" and secondCity == "Пенза"):
+  print("No")
+elif(firstCity == "Пенза" and secondCity != "Пенза")or(firstCity == "Тула" and secondCity != "Тула"):
+  print("yes")

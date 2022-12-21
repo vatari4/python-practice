ead91be1be9b1eb86eb254c9b663efe48ea1bbbb
VenomFeed <93274199+vatari4@users.noreply.github.com>
2022-12-22 06:20:16 +0700

    Update 4.6.py

--- python4/4.6.py
+++ python4/4.6.py
@@ -1,3 +1,3 @@
 numbers = int(input())
-for i in range(0, numbers + 1):
-  print(i, end=" ")
+for i in range(0, numbers + 1): #перебирает от 0 до n
+  print(i, end=" ")             #выводит всё в 1 строку

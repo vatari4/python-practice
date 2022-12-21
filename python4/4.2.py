ae50e5c436d587c5abc8b5fc881778ffa8506e83
VenomFeed <93274199+vatari4@users.noreply.github.com>
2022-12-22 05:47:33 +0700

    Update 4.2.py

--- python4/4.2.py
+++ python4/4.2.py
@@ -1,3 +1,3 @@
-string = input()
-for i in range(int(input())):
+string = input()                    #вводится строка
+for i in range(int(input())):       #увеличивать на n-e кол-во раз переменную string
   print(string)

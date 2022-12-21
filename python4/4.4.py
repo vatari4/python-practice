b9b926fb9ccd4531f75c580a7417d9261dd2dfaf
VenomFeed <93274199+vatari4@users.noreply.github.com>
2022-12-22 06:13:16 +0700

    Create 4.4.py

---
+++ python4/4.4.py
@@ -0,0 +1,5 @@
+result = 1
+factorial = int(input())
+for i in range(1, factorial + 1):  #запись чисел факторила
+  result *= i                      #создание факториала путём умножения чисел
+  print(result)                    

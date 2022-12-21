81ecea0d8739b806f9d4ba6184dc2203370c19cb
VenomFeed <93274199+vatari4@users.noreply.github.com>
2022-12-22 05:42:18 +0700

    Update 4.1.py

--- python4/4.1.py
+++ python4/4.1.py
@@ -1,6 +1,6 @@
 temp = float(input())
 days = 0
-while temp <22:
+while temp <22:  #пока температура ниже 22 цикл будет постоянно повторяться
   days +=1
   temp = float(input())
-print(days // 7)
+print(days // 7)    # показывает какой день в недели температура была <=22

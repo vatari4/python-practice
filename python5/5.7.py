f055412a42454a292ffb36044d82e5c4f42f841b
VenomFeed <93274199+vatari4@users.noreply.github.com>
2022-12-22 22:51:41 +0300

    Create 5.7.py

---
+++ python5/5.7.py
@@ -0,0 +1,17 @@
+e = True            #евразия
+o = False           #остазия
+n = int(input())
+for i in range(n):    #переборка i в диапозоне от 0 до n-1
+   war = input()
+   if war == 'С кем война?':
+       if e:
+           print('Евразия')
+       else:
+           print('Остазия')
+   elif war == 'С кем мир?':
+       if not o:
+           print('Остазия')
+       else:
+           print('Евразия')
+   else:
+       e, o = o, e

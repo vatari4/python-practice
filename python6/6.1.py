5e1d961994a8d94369971eb300154ec7427b490a
VenomFeed <93274199+vatari4@users.noreply.github.com>
2022-12-23 10:49:35 +0300

    Update 6.1.py

--- python6/6.1.py
+++ python6/6.1.py
@@ -1,15 +1,15 @@
-list1 = set()
-list2 = set()
+list1 = set()                    #создание 1 листа 
+list2 = set()                    #создание 2 листа
 words = input()
-while words != '':
+while words != '':               #пока не попадётся пустая строка будет добавлять в лист значения
    list1.add(words)
    words = input()
 list1.add(words)
 words1 = input()
-while words1 != '':
+while words1 != '':              #пока не попадётся пустая строка будет добавлять в лист значения
    list2.add(words1)
    words1 = input()
-intersection = list1 & list2
+intersection = list1 & list2        #ищет пересечения между листами
 if not intersection:
    print('EMPTY')
 else:

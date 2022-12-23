05bec31b717ee2aa12f47c40f7493a9b39f5d58f
VenomFeed <93274199+vatari4@users.noreply.github.com>
2022-12-23 11:19:23 +0300

    Update 6.2.py

--- python6/6.2.py
+++ python6/6.2.py
@@ -1,16 +1,16 @@
-n = int(input())
-s = set()
+n = int(input())                    #вводится число городов
+s = set()                           #вводится города
 c = 0
 k = 0
 while k == 0:
    if c == n:
-       d = input()
+       d = input()              #новый город
        break
    else:
-       a = input()
-   c = c + 1
-   s.add(a)
-if d not in s:
+       a = input()               #новый города
+   c = c + 1            
+   s.add(a)                       #добавляет внутрь листа новый город
+if d not in s:                   #если нового города нет в списке выводит ok
    print('OK')
 else:
    print('TRY ANOTHER')

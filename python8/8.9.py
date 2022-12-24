f837d54033e9b3cee3d849d0f9e401c1616f4359
VenomFeed <93274199+vatari4@users.noreply.github.com>
2022-12-24 22:11:36 +0300

    Create 8.9.py

---
+++ python8/8.9.py
@@ -0,0 +1,12 @@
+N = int(input('N = '))
+text = [input() for _ in range(N)]
+
+for t in text:
+   if t[:4] == '####':
+       continue
+
+   if t[:2] == '%%':
+       print(t[2:])
+   else:
+       print(t)
+

678c1a6015bc229c5fe658ee30bcfdbe512c4ff4
VenomFeed <93274199+vatari4@users.noreply.github.com>
2022-12-26 16:15:34 +0700

    Create 14.10.py

---
+++ python14/14.10.py
@@ -0,0 +1,12 @@
+ef golden_ratio(i):
+    a, b = 0, 1
+    n = 1
+    while n <= i:
+        a, b = b, a + b
+        n += 1
+        if n == i - 1:
+            b1 = b
+    return(b1 / b)
+ 
+ 
+print(golden_ratio(int(input())))

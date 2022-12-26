848e8c737f9a9bf90ab04e0e7bd6eeb659106237
VenomFeed <93274199+vatari4@users.noreply.github.com>
2022-12-26 16:08:07 +0700

    Update 14.4.py

--- python14/14.4.py
+++ python14/14.4.py
@@ -1,5 +1,5 @@
 def who_are_you_and_hello():
     name = input()
-    while not name.isalpha() or not name.istitle():
+    while not name.isalpha() or not name.istitle():             #если первая буква не заглавная и есть какие либо символы, программа не выведет его как name
         name = input()
     print(f'Привет, {name}!')

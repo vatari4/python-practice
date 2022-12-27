c6aa854d813ca2d81470549c7f186134508cb069
VenomFeed <93274199+vatari4@users.noreply.github.com>
2022-12-27 11:04:49 +0700

    Update 24.1.py

--- python24/24.1.py
+++ python24/24.1.py
@@ -1,14 +1,13 @@
 from PIL import Image
- 
- 
+
+
 def twist_image(input_file_name, output_file_name):
-    im = Image.open(input_file_name)
-    px = im.load()
-    s, h = im.size
-    for x in range(s // 2):
-        for y in range(h):
-            r, g, b = px[x, y]
-            r1, g1, b1 = px[s // 2 + x, y]
-            px[x, y] = r1, g1, b1
-            px[s // 2 + x, y] = r, g, b
-    im.save(output_file_name)
+ im = Image.open(input_file_name)
+ x, y = im.size
+ x1 = int(x / 2)
+ y1 = int(y / 2)
+ p1 = im.crop((0, 0, x1, y))
+ p2 = im.crop((x1, 0, x, y))
+ im.paste(p2, (0, 0))
+ im.paste(p1, (x1, 0))
+ im.save(output_file_name)

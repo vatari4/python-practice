a3058323080120c5059862e92ae17b76ca1d4e26
VenomFeed <93274199+vatari4@users.noreply.github.com>
2022-12-27 11:24:47 +0700

    Create 16.2.py

---
+++ python16/16.2.py
@@ -0,0 +1,24 @@
+def discriminant(a,b,c):
+  return b**2 - 4*a*c
+ 
+def larger_root(p,q):
+  D = discriminant(1, p,q)
+  if D > 0:
+    return (-p + D ** 0.5) / 2
+ 
+def smaller_root(p,q):
+  D = discriminant(1, p, q)
+  if D > 0:
+    return (-p - D ** 0.5) / 2
+ 
+def main():
+  p = float(input())
+  q = float(input())
+  print(discriminant(1, p, q))
+  print(smaller_root(p, q), larger_root(p, q))
+ 
+ 
+print(discriminant(1,2,1))
+print(smaller_root(3,1))
+print(larger_root(3, 1))
+main()
